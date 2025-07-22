e4ea951b370f0510631b866766e76241579d412c
DLIFE21 <darlin45franco.brito@gmail.com>
2025-07-22 15:27:52 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('pago').addEventListener('change', function () {
-    const tarjetaCampos = document.getElementById('tarjetaCampos');
-    if (this.value === 'tarjeta') {
-        tarjetaCampos.style.display = 'block';
-    } else {
-        tarjetaCampos.style.display = 'none';
-    }
-});
